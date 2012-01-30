82cbd0bf9b40d98ad2d1f19f20b3ae3be9460663
Markus Gasser <markus.gasser@coresystems.ch>
2012-01-30 23:15:50 +0100

    New project setup (static library instead of framework)

--- rgmock/RGMock.h
+++ rgmock/RGMock.h
@@ -1,9 +1,13 @@
 //
-//  RGMock.h
+//  rgmock.h
 //  rgmock
 //
 //  Created by Markus Gasser on 30.01.12.
-//  Copyright (c) 2012 coresystems ag. All rights reserved.
+//  Copyright 2012 coresystems ag. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+@interface rgmock : NSObject
+
+@end
