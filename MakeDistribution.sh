01557877e20ce9182722684f5285aaad9b7e7dd0
Markus Gasser <markus.gasser@konoma.ch>
2013-11-10 19:55:21 +0100

    Add Distribution directory if not yet there

--- MakeDistribution.sh
+++ MakeDistribution.sh
@@ -1,6 +1,7 @@
 set -e
 
 echo "Building Framework..."
+mkdir -p Distribution
 xcodebuild -scheme "Universal Framework" -configuration "Debug" > Distribution/build.log
 open Distribution/
 echo "Done."
