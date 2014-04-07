6629d95c3e1cfa657e11854ffbf769c9c16032eb
Markus Gasser <markus.gasser@konoma.ch>
2014-04-07 21:24:14 +0200

    Add OS X targets for the examples

---
+++ Examples/Sources/Dummy.m
@@ -0,0 +1,14 @@
+//
+//  Dummy.m
+//  Examples
+//
+//  Created by Markus Gasser on 07.04.14.
+//  Copyright (c) 2014 konoma GmbH. All rights reserved.
+//
+
+#import "Dummy.h"
+
+
+@implementation Dummy
+
+@end
