eb428532a721a5b1e1a71b7c2a74e5c6adefba8a
Markus Gasser <markus.gasser@konoma.ch>
2014-04-10 23:48:41 +0200

    Add basic match examples

--- Tests/ExamplesVerify.m
+++ Tests/ExamplesVerify.m
@@ -29,61 +29,6 @@
 
 #pragma mark - Basic Verification
 
-- (void)testVerifySimpleMethodCall
-{
-    // you first use the mock  then verify that the desired method was called
-    
-    [mockArray removeAllObjects];
-    
-    match ([mockArray removeAllObjects]);
-}
-
-- (void)testVerifyWillFailIfMethodWasNeverCalled
-{
-    // if you verify a method that has never been called it will fail
-    
-    ThisWillFail({
-        match ([mockArray removeAllObjects]);
-    });
-}
-
-- (void)testTooManyInvocationsAreIgnoredByVerify
-{
-    // by default, verify will just check if a given method was at least called once
-    // any more invocations are simply ignored
-    
-    [mockArray removeAllObjects];
-    [mockArray removeAllObjects];
-    
-    match ([mockArray removeAllObjects]); // too many invocations are ok
-}
-
-- (void)testYouCanUseMultipleVerifyTestsToRequireMultipleCalls
-{
-    // if you wan to test that a method was called multiple times just repeat the verify call
-    // (you can also use a modifier to verify, see below)
-    
-    [mockArray removeAllObjects];
-    [mockArray removeAllObjects];
-    
-    match ([mockArray removeAllObjects]);
-    match ([mockArray removeAllObjects]); // verify that the method was called at least 2 times
-}
-
-- (void)testVerifyingSomethingMultpleTimesThatWasCalledOnceWillFail
-{
-    // from the example above follows that if you try to verify a call that was made only once
-    // multiple times, the second verify will fail
-    
-    [mockArray removeAllObjects];
-    
-    match ([mockArray removeAllObjects]);
-    
-    ThisWillFail({
-        match ([mockArray removeAllObjects]); // multiple verify calls will also expect multiple invocations
-    });
-}
-
 
 #pragma mark - Verification With Arguments
 
